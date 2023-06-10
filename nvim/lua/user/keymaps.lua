a0d532faafb30eb0a8afa13f06b0542495dab39d
Andy <amcdonald1307@protonmail.com>
2023-06-10 23:13:29 +0100

    Added neovim config

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+vim.keymap.set('v', 'y', 'myy`y')
+
+vim.keymap.set('v', 'p', '"_dP')
+
+vim.keymap.set('n', '<Leader>k', ':nohlseach<CR>')
+
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
+
+
